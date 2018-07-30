76afa6fa49d28e23bdebf2b38dc4e1c8057d2eda
mvboettcher <mvboettcher@gmail.com>
2018-07-30 15:56:47 -0400

    completed PUT route for resources

--- api/dal.js
+++ api/dal.js
@@ -1,39 +1,44 @@
-const PouchDB = require("pouchdb-core")
-PouchDB.plugin(require("pouchdb-adapter-http"))
-const { map, prop, merge } = require("ramda")
+const PouchDB = require('pouchdb-core')
+PouchDB.plugin(require('pouchdb-adapter-http'))
+const { map, prop, merge } = require('ramda')
 const COUCHDB_SERVER = process.env.COUCHDB_SERVER
 const COUCHDB_DBNAME = process.env.COUCHDB_DBNAME
 const DB_URL = `${COUCHDB_SERVER}${COUCHDB_DBNAME}`
 
 const db = new PouchDB(DB_URL)
-const { getAllDocs } = require("./dal-helper")
-const pkGenResource = require("./lib/pkGen-resource")
+const { getAllDocs } = require('./dal-helper')
+const pkGenResource = require('./lib/pkGen-resource')
 
 const getResources = () =>
   getAllDocs(db, {
     include_docs: true,
-    startkey: "resource_",
-    endkey: "resource_\ufff0"
+    startkey: 'resource_',
+    endkey: 'resource_\ufff0'
   })
 
 const postResource = resource => {
   //console.log(JSON.stringify(resource))
   const modifiedResource = merge(resource, {
     _id: pkGenResource(resource),
-    type: "resource"
+    type: 'resource'
   })
   return db.put(modifiedResource)
+}
+
+const putResource = resource => {
+  return db.put(resource)
 }
 
 const getCategories = () =>
   getAllDocs(db, {
     include_docs: true,
-    startkey: "category_",
-    endkey: "category_\ufff0"
+    startkey: 'category_',
+    endkey: 'category_\ufff0'
   })
 
 module.exports = {
   getResources,
   getCategories,
-  postResource
+  postResource,
+  putResource
 }
--- api/routes/resources.js
+++ api/routes/resources.js
@@ -1,7 +1,7 @@
 const NodeHTTPError = require('node-http-error')
-const { getResources, postResource } = require('../dal')
+const { getResources, postResource, putResource } = require('../dal')
 const bodyParser = require('body-parser')
-const { propOr, isEmpty, not } = require('ramda')
+const { propOr, isEmpty, not, concat } = require('ramda')
 const checkReqFields = require('../lib/check-required-fields')
 const missingFieldMsg = require('../lib/missing-field-msg')
 const cleanObj = require('../lib/clean-object')
@@ -14,6 +14,19 @@ const reqFields = [
   'purpose',
   'website'
 ]
+
+const allowedFields = concat(
+  [
+    'type',
+    'organization',
+    'contacts',
+    'primaryPhone',
+    'addresses',
+    'rank',
+    'faq'
+  ],
+  reqFields
+)
 
 const resourcesRoutes = app => {
   app.get('/', (req, res) => res.send('Welcome to the VET API'))
@@ -49,12 +62,49 @@ const resourcesRoutes = app => {
       next(new NodeHTTPError(400, missingFieldMsg(missingFields)))
       return
     }
-    const cleanResource = cleanObj(reqFields, newResource)
+    const cleanResource = cleanObj(allowedFields, newResource)
     //console.log("clean", cleanResource)
     postResource(cleanResource)
       .then(result => {
         console.log({ result })
         res.status(201).send(result)
+      })
+      .catch(err => new NodeHTTPError(err.status, err.message, err))
+  })
+
+  app.put('/resources', bodyParser.json(), (req, res, next) => {
+    const newResource = propOr({}, 'body', req)
+    // console.log(JSON.stringify(newResource))
+    if (isEmpty(newResource)) {
+      next(
+        new NodeHTTPError(
+          400,
+          'No valid JSON document was provided in the request body.'
+        )
+      )
+      return
+    }
+    // console.log("new", newResource)
+    const missingFields = checkReqFields(
+      concat(['_id', '_rev'], reqFields),
+      newResource
+    )
+    console.log(missingFields)
+    //console.log(not(isEmpty(missingFields)))
+    if (not(isEmpty(missingFields))) {
+      //console.log(missingFieldMsg(missingFields))
+      next(new NodeHTTPError(400, missingFieldMsg(missingFields)))
+      return
+    }
+    const cleanResource = cleanObj(
+      concat(allowedFields, ['_id', '_rev']),
+      newResource
+    )
+    //console.log("clean", cleanResource)
+    putResource(cleanResource)
+      .then(result => {
+        console.log({ result })
+        res.status(200).send(result)
       })
       .catch(err => new NodeHTTPError(err.status, err.message, err))
   })
