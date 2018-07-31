1ed983c5fc5f6ae908360c74884b1a51471edb19
tripott <tripott@gmail.com>
2018-07-31 13:36:25 -0400

    added home page with route and icons and junk

---
+++ app/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react"
+import "./App.css"
+import { Switch, BrowserRouter, Route } from "react-router-dom"
+import Home from "./pages/home"
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    )
+  }
+}
+
+export default App
---
+++ app/src/pages/home.js
@@ -0,0 +1,23 @@
+import React from "react"
+import Typography from "@material-ui/core/Typography"
+import Button from "@material-ui/core/Button"
+
+const Home = () => (
+  <div style={{ padding: 48 }}>
+    <center>
+      <img alt="home icon" src="/tcvsn-ico.png" />
+      <div style={{ paddingTop: 12 }}>
+        <Typography variant="display1">
+          Welcome to Veteran Support Resources App
+        </Typography>
+      </div>
+      <div style={{ paddingTop: 12 }}>
+        <Button variant="raised" color="primary">
+          Categories
+        </Button>
+      </div>
+    </center>
+  </div>
+)
+
+export default Home
