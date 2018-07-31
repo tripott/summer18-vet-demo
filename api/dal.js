2cfe66921a20f624ee240f3afed10f89e31a15e0
Peter Estes <peternigelestes@gmail.com>
2018-07-31 09:58:52 -0400

    resources-filter query string in dal

--- api/dal.js
+++ api/dal.js
@@ -1,20 +1,22 @@
-const PouchDB = require('pouchdb-core')
-PouchDB.plugin(require('pouchdb-adapter-http'))
-const { map, prop, merge } = require('ramda')
+const PouchDB = require("pouchdb-core")
+PouchDB.plugin(require("pouchdb-adapter-http"))
+const { map, prop, merge, split, not, isEmpty } = require("ramda")
 const COUCHDB_SERVER = process.env.COUCHDB_SERVER
 const COUCHDB_DBNAME = process.env.COUCHDB_DBNAME
 const DB_URL = `${COUCHDB_SERVER}${COUCHDB_DBNAME}`
 
 const db = new PouchDB(DB_URL)
-const { getAllDocs } = require('./dal-helper')
-const pkGenResource = require('./lib/pkGen-resource')
+const { getAllDocs } = require("./dal-helper")
+const pkGenResource = require("./lib/pkGen-resource")
 
-const getResources = () =>
+const getResources = query => {
+  const [key, value] = not(isEmpty(query)) ? split(":", query) : ""
   getAllDocs(db, {
     include_docs: true,
-    startkey: 'resource_',
-    endkey: 'resource_\ufff0'
+    startkey: "resource_",
+    endkey: "resource_\ufff0"
   })
+}
 
 const getResource = id => db.get(id)
 
@@ -22,7 +24,7 @@ const postResource = resource => {
   //console.log(JSON.stringify(resource))
   const modifiedResource = merge(resource, {
     _id: pkGenResource(resource),
-    type: 'resource'
+    type: "resource"
   })
   return db.put(modifiedResource)
 }
@@ -36,8 +38,8 @@ const deleteResource = resource => db.remove(resource)
 const getCategories = () =>
   getAllDocs(db, {
     include_docs: true,
-    startkey: 'category_',
-    endkey: 'category_\ufff0'
+    startkey: "category_",
+    endkey: "category_\ufff0"
   })
 
 module.exports = {
