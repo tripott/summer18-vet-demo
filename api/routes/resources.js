3ecba8df5fa0cfc6ad3cad825feed8b42a9e7da0
Will Adkins <wkadki01@gmail.com>
2018-07-31 11:47:19 -0400

    minor merge changes, full resource functionality

--- api/routes/resources.js
+++ api/routes/resources.js
@@ -1,45 +1,45 @@
-const NodeHTTPError = require("node-http-error")
+const NodeHTTPError = require('node-http-error')
 const {
   getResources,
   getResource,
   postResource,
   putResource,
   deleteResource
-} = require("../dal")
-const bodyParser = require("body-parser")
-const { propOr, isEmpty, not, concat, pathOr } = require("ramda")
-const checkReqFields = require("../lib/check-required-fields")
-const missingFieldMsg = require("../lib/missing-field-msg")
-const cleanObj = require("../lib/clean-object")
+} = require('../dal')
+const bodyParser = require('body-parser')
+const { propOr, isEmpty, not, concat, pathOr } = require('ramda')
+const checkReqFields = require('../lib/check-required-fields')
+const missingFieldMsg = require('../lib/missing-field-msg')
+const cleanObj = require('../lib/clean-object')
 
 const reqFields = [
-  "categoryId",
-  "name",
-  "formalName",
-  "shortDesc",
-  "purpose",
-  "website"
+  'categoryId',
+  'name',
+  'formalName',
+  'shortDesc',
+  'purpose',
+  'website'
 ]
 
 const allowedFields = concat(
   [
-    "type",
-    "organization",
-    "contacts",
-    "primaryPhone",
-    "addresses",
-    "rank",
-    "faq"
+    'type',
+    'organization',
+    'contacts',
+    'primaryPhone',
+    'addresses',
+    'rank',
+    'faq'
   ],
   reqFields
 )
 
 const resourcesRoutes = app => {
-  app.get("/", (req, res) => res.send("Welcome to the VET API"))
+  app.get('/', (req, res) => res.send('Welcome to the VET API'))
 
-  app.get("/resources", (req, res, next) => {
+  app.get('/resources', (req, res, next) => {
     // console.log("inside server.js hit /resources route")
-    const query = pathOr("", ["query", "filter"], req)
+    const query = pathOr('', ['query', 'filter'], req)
     //console.log("query", query)
 
     getResources(query)
@@ -49,8 +49,8 @@ const resourcesRoutes = app => {
       })
   })
 
-  app.get("/resources/:id", (req, res, next) => {
-    const resourceId = pathOr("", ["params", "id"], req)
+  app.get('/resources/:id', (req, res, next) => {
+    const resourceId = pathOr('', ['params', 'id'], req)
     getResource(resourceId)
       .then(resource => res.status(200).send(resource))
       .catch(err => {
@@ -58,14 +58,14 @@ const resourcesRoutes = app => {
       })
   })
 
-  app.post("/resources", bodyParser.json(), (req, res, next) => {
-    const newResource = propOr({}, "body", req)
+  app.post('/resources', bodyParser.json(), (req, res, next) => {
+    const newResource = propOr({}, 'body', req)
     // console.log(JSON.stringify(newResource))
     if (isEmpty(newResource)) {
       next(
         new NodeHTTPError(
           400,
-          "No valid JSON document was provided in the request body."
+          'No valid JSON document was provided in the request body.'
         )
       )
       return
@@ -89,21 +89,21 @@ const resourcesRoutes = app => {
       .catch(err => new NodeHTTPError(err.status, err.message, err))
   })
 
-  app.put("/resources", bodyParser.json(), (req, res, next) => {
-    const newResource = propOr({}, "body", req)
+  app.put('/resources/:id', bodyParser.json(), (req, res, next) => {
+    const newResource = propOr({}, 'body', req)
     // console.log(JSON.stringify(newResource))
     if (isEmpty(newResource)) {
       next(
         new NodeHTTPError(
           400,
-          "No valid JSON document was provided in the request body."
+          'No valid JSON document was provided in the request body.'
         )
       )
       return
     }
     // console.log("new", newResource)
     const missingFields = checkReqFields(
-      concat(["_id", "_rev"], reqFields),
+      concat(['_id', '_rev'], reqFields),
       newResource
     )
     //console.log(missingFields)
@@ -114,7 +114,7 @@ const resourcesRoutes = app => {
       return
     }
     const cleanResource = cleanObj(
-      concat(allowedFields, ["_id", "_rev"]),
+      concat(allowedFields, ['_id', '_rev']),
       newResource
     )
     //console.log("clean", cleanResource)
@@ -126,8 +126,8 @@ const resourcesRoutes = app => {
       .catch(err => new NodeHTTPError(err.status, err.message, err))
   })
 
-  app.delete("/resources", bodyParser.json(), (req, res, next) => {
-    const resource = propOr({}, "body", req)
+  app.delete('/resources/:id', bodyParser.json(), (req, res, next) => {
+    const resource = propOr({}, 'body', req)
     deleteResource(resource)
       .then(result => res.status(200).send(result))
       .catch(err => next(new NodeHTTPError(err.status, err.message, err)))
