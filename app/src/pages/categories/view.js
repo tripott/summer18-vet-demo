bbd249461f51e71a838b20d12a842868fd241cee
Brian Liotti <brianliotti@bliottis-mac-osx.local>
2018-08-01 14:41:18 -0400

    view cate. with Card Expansion'

--- app/src/pages/categories/view.js
+++ app/src/pages/categories/view.js
@@ -1,43 +1,111 @@
-import React from "react";
-import Component from "@reactions/component";
-import { Link } from "react-router-dom";
-import { getCategory } from "../../action-creators/categories";
-import categoryListItems from "../../components/categoryListItems";
-import Typography from "@material-ui/core/Typography";
-import { ListItem, Icon, ListItemText, List } from "@material-ui/core";
-import { connect } from "react-redux";
+import React from "react"
+import { withStyles } from "@material-ui/core/styles"
+import { getCategory } from "../../action-creators/categories"
+import {
+  Icon,
+  Card,
+  CardHeader,
+  CardContent,
+  CardActions,
+  Collapse,
+  IconButton,
+  Typography
+} from "@material-ui/core"
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
+import { connect } from "react-redux"
+import classnames from "classnames"
 
-const ViewCategory = props => (
-  <Component didMount={() => props.getCategory(props.match.params.id)}>
-    {() => (
+const styles = theme => ({
+  card: {
+    maxWidth: 400
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%" // 16:9
+  },
+  actions: {
+    display: "flex"
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest
+    }),
+    marginLeft: "auto",
+    [theme.breakpoints.up("sm")]: {
+      marginRight: -8
+    }
+  },
+  expandOpen: {
+    transform: "rotate(180deg)"
+  }
+})
+
+class ViewCategory extends React.Component {
+  state = { expanded: false }
+
+  handleExpandClick = () => {
+    this.setState(state => ({ expanded: !state.expanded }))
+  }
+
+  componentDidMount() {
+    this.props.getCategory(this.props.match.params.id)
+    // props.getCategory(props.match.params.id)
+  }
+
+  render() {
+    const { classes, currentCategory, match } = this.props
+
+    return (
       <React.Fragment>
-        {props.match.params.id === props.currentCategory._id ? (
-          <ListItem>
-            <Icon style={{ color: "grey" }}>{props.currentCategory.icon}</Icon>
-            <ListItemText
-              primary={props.currentCategory.name}
-              secondary={props.currentCategory.shortDesc}
+        {match.params.id === currentCategory._id ? (
+          <Card>
+            <CardHeader
+              avatar={
+                <Icon aria-label={currentCategory.name}>
+                  {currentCategory.icon}
+                </Icon>
+              }
+              title={currentCategory.name}
+              subheader={currentCategory.shortDesc}
             />
-          </ListItem>
+
+            <CardActions disableActionSpacing>
+              <IconButton
+                className={classnames(classes.expand, {
+                  [classes.expandOpen]: this.state.expanded
+                })}
+                onClick={this.handleExpandClick}
+                aria-expanded={this.state.expanded}
+                aria-label="Show more"
+              >
+                <ExpandMoreIcon />
+              </IconButton>
+            </CardActions>
+            <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+              <CardContent>
+                <Typography paragraph>{currentCategory.desc}</Typography>
+              </CardContent>
+            </Collapse>
+          </Card>
         ) : (
           <p>Loading</p>
         )}
       </React.Fragment>
-    )}
-  </Component>
-);
-
+    )
+  }
+}
 const mapStateToProps = state => {
-  return { currentCategory: state.currentCategory };
-};
+  return { currentCategory: state.currentCategory }
+}
 
 const mapActionToProps = dispatch => {
-  return { getCategory: id => dispatch(getCategory(id)) };
-};
+  return { getCategory: id => dispatch(getCategory(id)) }
+}
 
 const connector = connect(
   mapStateToProps,
   mapActionToProps
-);
+)
 
-export default connector(ViewCategory);
+export default withStyles(styles)(connector(ViewCategory))
